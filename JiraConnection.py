2770613fca307a2f7616207ca2007360d95f0be4
Sampath Mummaneni <sam@Sampaths-MacBook-Pro.local>
2023-08-04 01:26:28 +0530

    Initial Commit

---
+++ JiraConnection.py
@@ -0,0 +1,245 @@
+import requests
+from requests.sessions import Session
+import streamlit as st
+from typing import Dict, Union, List, Any
+from streamlit.connections import ExperimentalBaseConnection
+import json
+import pandas as pd
+from requests.exceptions import HTTPError
+
+
+class JiraConnection(ExperimentalBaseConnection[requests.Session]):
+    """
+    A custom Streamlit connection for interacting with the Jira API using the `requests` library.
+
+    Parameters:
+        connection_name (str): The name of the connection.
+        base_url (str): The base URL of the Jira instance (e.g., "https://your-jira-instance.atlassian.net").
+        **kwargs: Additional keyword arguments to be passed to the parent class constructor.
+
+    Attributes:
+        base_url (str): The base URL of the Jira instance.
+
+    Raises:
+        ValueError: If invalid or missing credentials are provided.
+
+    Example:
+        jira_connection = JiraConnection(
+            connection_name="my_jira",
+            base_url="https://your-jira-instance.atlassian.net",
+            credentials={"username": "your_username", "password": "your_password"},
+        )
+    """
+
+    def __init__(self, connection_name: str, base_url: str, **kwargs):
+        self.base_url = base_url
+        super().__init__(connection_name, **kwargs)
+
+    def _connect(self, **kwargs) -> Session:
+        """
+        Establishes a connection to the Jira API using the provided credentials.
+
+        Parameters:
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            Session: A new requests.Session object with authentication credentials.
+
+        Raises:
+            ValueError: If invalid or missing credentials are provided.
+
+        Example:
+            # Connecting with valid credentials
+            session = self._connect(credentials={"username": "your_username", "password": "your_password"})
+        """
+        if "credentials" in kwargs:
+            credentials = kwargs.pop("credentials")
+        else:
+            credentials = self._secrets["credentials"]
+        username = credentials["username"]
+        password = credentials["password"]
+        if credentials and username and password:
+            session = requests.Session()
+            session.auth = (username, password)
+            return session
+        else:
+            raise ValueError("ERROR: Invalid or missing credentials")
+
+    def cursor(self):
+        """
+        Returns the underlying requests.Session object used for making API requests.
+
+        Returns:
+            Session: The underlying requests.Session object.
+
+        Example:
+            # Getting the cursor to make direct API requests
+            session = jira_connection.cursor()
+        """
+        return self._instance
+
+    def query(
+        self, endpoint: str, params: Dict[str, Any] = None, ttl=600, **kwargs
+    ) -> List[Dict[str, Any]]:
+        """
+        Executes an API GET request to the specified Jira endpoint.
+
+        Parameters:
+            endpoint (str): The API endpoint to be appended to the base URL.
+            params (Dict[str, Any], optional): Query parameters to be sent with the request.
+            ttl (int, optional): Time to live in seconds for caching the response (default: 600 seconds).
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            List[Dict[str, Any]]: The JSON response data as a list of dictionaries.
+
+        Example:
+            # Querying the Jira API for all projects
+            projects = jira_connection.query(endpoint="/rest/api/3/project")
+        """
+
+        @st.cache_data(ttl=ttl)
+        def _query(endpoint: str, params: Dict[str, Any] = None, **kwargs):
+            url = self.base_url + endpoint
+            response = self.cursor().get(url, params=params)
+            response.raise_for_status()  # Raise an error if the request fails
+            return response.json()
+
+        return _query(endpoint, params, **kwargs)
+
+    def query_projects(
+        self, params: Dict[str, Any] = None, ttl=600, **kwargs
+    ) -> List[Dict[str, Any]]:
+        """
+        Retrieves a list of Jira projects using the 'query' method.
+
+        Parameters:
+            params (Dict[str, Any], optional): Query parameters to be sent with the request.
+            ttl (int, optional): Time to live in seconds for caching the response (default: 600 seconds).
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            List[Dict[str, Any]]: The JSON response data as a list of project dictionaries.
+
+        Example:
+            # Querying Jira API for all projects
+            projects = jira_connection.query_projects()
+        """
+
+        @st.cache_data(ttl=ttl)
+        def _query(params: Dict[str, Any] = None, **kwargs):
+            url = self.base_url + "/rest/api/3/project"
+            response = self.cursor().get(url, params=params)
+            response.raise_for_status()
+            return response.json()
+
+        return _query(params, **kwargs)
+
+    def query_issue(
+        self, issue_id: str, params: Dict[str, Any] = None, ttl=600, **kwargs
+    ):
+        """
+        Retrieves information about a specific Jira issue using the 'query' method.
+
+        Parameters:
+            issue_id (str): The ID of the Jira issue to retrieve information about.
+            params (Dict[str, Any], optional): Query parameters to be sent with the request.
+            ttl (int, optional): Time to live in seconds for caching the response (default: 600 seconds).
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            Dict[str, Any]: The JSON response data as a dictionary representing the issue.
+
+        Example:
+            # Querying Jira API for information about a specific issue
+            issue_id = "ISSUE-123"
+            issue_data = jira_connection.query_issue(issue_id)
+        """
+
+        @st.cache_data(ttl=ttl)
+        def _query(params: Dict[str, Any] = None, **kwargs):
+            url = self.base_url + "/rest/api/3/issue/" + issue_id
+            try:
+                response = self.cursor().get(url, params=params)
+                response.raise_for_status()
+                return response.json()
+            except HTTPError as e:
+                # Handle the 404 error or other HTTP errors
+                return None
+
+        return _query(params, **kwargs)
+
+    def query_jql(
+        self,
+        jql_query: str,
+        ttl: int = 600,
+        expand=["changelog"],
+        fields=["summary", "status"],
+        max_results=10,
+        start_at=0,
+        return_type="count",
+        **kwargs,
+    ) -> List[Dict[str, Union[str, int]]]:
+        """
+        Executes a Jira Query Language (JQL) query and retrieves matching issues.
+
+        Parameters:
+            jql_query (str): The JQL query string to be executed.
+            ttl (int, optional): Time to live in seconds for caching the response (default: 600 seconds).
+            expand (List[str], optional): A list of fields to expand in the Jira issue (default: ["changelog"]).
+            fields (List[str], optional): A list of fields to include in the search results (default: ["summary", "status"]).
+            max_results (int, optional): The maximum number of results to retrieve (default: 10).
+            start_at (int, optional): The index of the first result to retrieve (default: 0).
+            return_type (str, optional): The type of data to return. Possible values are "count" (returns the total number of issues matching the query),
+                                         "json" (returns a list of issue dictionaries), and "dataframe" (returns a Pandas DataFrame of the issues) (default: "count").
+            **kwargs: Additional keyword arguments.
+
+        Returns:
+            List[Dict[str, Union[str, int]]]: The JSON response data as a list of dictionaries representing the matching issues.
+
+        Raises:
+            ValueError: If 'jql_query' is empty or not provided.
+
+        Example:
+            # Executing a JQL query to find all issues in a project
+            jql_query = 'project = "MYPROJECT"'
+            issues = jira_connection.query_jql(jql_query, return_type="json")
+        """
+        if not jql_query:
+            raise ValueError("ERROR: 'jql_query' is required")
+
+        @st.cache_data(ttl=ttl)
+        def _query(jql_query: str, **kwargs):
+            payload = json.dumps(
+                {
+                    "expand": expand,
+                    "fields": fields,
+                    "fieldsByKeys": False,
+                    "jql": jql_query,
+                    "maxResults": max_results,
+                    "startAt": start_at,
+                }
+            )
+            try:
+                response = self.cursor().post(
+                    self.base_url + "/rest/api/3/search",
+                    data=payload,
+                    headers={"Content-Type": "application/json"},
+                )
+                response.raise_for_status()  # Raise an exception for non-2xx responses
+                data = response.json()
+                return data
+            except HTTPError as e:
+                # Handle the 404 error or other HTTP errors
+                return {"error": f"HTTP Error: {e}"}
+
+        if return_type == "count":
+            return _query(jql_query, **kwargs)["total"]
+        elif return_type == "json":
+            return _query(jql_query, **kwargs)["issues"]
+        elif return_type == "dataframe":
+            issues = _query(jql_query, **kwargs)["issues"]
+            df = pd.json_normalize(issues)
+            return df
+        else:
+            return {"error": "Invalid return_type"}
---
+++ app.py
@@ -0,0 +1,158 @@
+from math import sin
+import streamlit as st
+from JiraConnection import JiraConnection
+import plotly.express as px
+
+
+st.set_page_config(layout="wide")
+
+st.markdown("""#### Working with Jira Cloud API using `ExperimentalBaseConnection`""")
+
+tab1, tab2 = st.tabs(["Dashboard", "Credential Setup"])
+
+
+conn = st.experimental_connection(
+    "jira",
+    type=JiraConnection,
+    base_url="https://sam1120.atlassian.net",
+    credentials={
+        "username": st.secrets["credentials"]["username"],
+        "password": st.secrets["credentials"]["password"],
+    },
+)
+
+with tab1:
+    st.title("Jira Dashboard")
+
+    # Query the Jira API using the search endpoint
+    st.markdown("### Single Issue view")
+    col_issue, col_detail = st.columns(2)
+
+    with col_issue:
+        issue_id = st.text_input(
+            "Enter an issue ID",
+            "test-1",
+            help="Enter an issue ID, try test-5 for example",
+            placeholder="Enter an issue ID, try test-5 for example",
+        )
+        st.markdown(
+            "Try using `test-5`, `test-6`, `test-7` to test the Single issue view"
+        )
+    with col_detail:
+        if issue_id:
+            single_issue = conn.query_issue(f"{issue_id}", ttl=0)
+            if single_issue:
+                summary = single_issue["fields"]["summary"]
+                if single_issue["fields"]["assignee"]:
+                    assignee = single_issue["fields"]["assignee"]["displayName"]
+                else:
+                    # Handle the case when single_issue is None or the keys are missing.
+                    assignee = "Unassigned"  # Or any default value that suits your application.
+
+                status = single_issue["fields"]["status"]["name"]
+                st.dataframe(
+                    {"summary": summary, "assignee": assignee, "status": status},
+                    width=600,
+                )
+            else:
+                st.error("Issue not found")
+
+    results_df = conn.query_jql(
+        "created >= -30d order by created ASC",
+        return_type="dataframe",
+        fields=["summary", "status", "assignee"],
+        ttl=0,
+    )
+    if len(results_df) > 0:
+        issues = results_df[
+            [
+                "key",
+                "fields.summary",
+                "fields.status.name",
+                "fields.assignee.displayName",
+            ]
+        ].rename(
+            columns={
+                "fields.summary": "summary",
+                "fields.status.name": "status",
+                "fields.assignee.displayName": "assignee",
+            }
+        )
+
+        status_count = (
+            issues.groupby("status").count().reset_index().drop("summary", axis=1)
+        )
+        st.write("----")
+        # Display the results
+        st.markdown("### Key Metrics based on the JQL using `JiraConnection.query_jql`")
+        col1, col2, col3, col4 = st.columns(4)
+        with col1:
+            st.metric(label="Total Issues", value=issues.shape[0])
+        with col2:
+            st.metric(
+                label="Completed",
+                value=status_count[status_count["status"] == "Done"]["key"].values[0],
+            )
+        with col3:
+            st.metric(
+                label="In Progress",
+                value=status_count[status_count["status"] == "In Progress"][
+                    "key"
+                ].values[0],
+            )
+        with col4:
+            number_projects = len(conn.query_projects())
+            st.metric(label="Projects", value=number_projects)
+        col5, col6 = st.columns(2, gap="medium")
+
+        with col5:
+            st.write("----")
+            st.markdown("### Status Breakdown")
+            fig = px.pie(status_count, values="key", names="status", width=600)
+            fig.update_layout(
+                legend=dict(
+                    orientation="h",
+                    yanchor="bottom",
+                    y=-0.1,
+                    xanchor="center",
+                    x=0.5,
+                    bgcolor="rgba(0, 0, 0, 0)",
+                )
+            )
+            st.plotly_chart(fig, use_container_width=True)
+        with col6:
+            st.write("----")
+            st.markdown("### Unassigned Backlog")
+            st.dataframe(
+                issues[issues["assignee"].isnull()].drop("assignee", axis=1),
+                width=600,
+            )
+    else:
+        st.error("No issues found")
+with tab2:
+    st.title("Credential setup")
+    st.markdown("### Using kwargs")
+    st.code(
+        """
+conn = st.experimental_connection(
+    "jira",
+    type=JiraConnection,
+    base_url="https://jira_wbsite.atlassian.net",
+    credentials={
+        "username": "user@email.com",
+        "password": "password_or_api_token",
+    },
+)
+""",
+        language="python",
+    )
+
+    st.markdown("### Using streamlit secrets")
+    st.code(
+        """
+[credentials]
+username = "user@email.com" # email id of the jira user
+password = "password_or_api_token" # password or api token of the jira user
+""",
+        language="toml",
+    )
